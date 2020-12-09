6687e3b2e68b4d620693ef38464e683164349623
Lev Kochin <rat.ratio@yandex.ru>
2020-12-09 17:35:02 +0600

    Create new models

---
+++ Rolodex/Models/RolodexContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rolodex.Models
+{
+    public class RolodexContext : DbContext
+    {
+        public DbSet<RolodexModel> Rolodexes { get; set; }
+        public DbSet<MnemonicImage> MnemonicImages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+           => options.UseSqlite("Data Source=Rolodex.Db");
+    }
+}
